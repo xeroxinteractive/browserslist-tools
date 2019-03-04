4933af385c6ca93934adf2b6d2fbd01c192e981f
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-03-04 14:55:50 +0000

    Fixed browserslist import

--- source/index.ts
+++ source/index.ts
@@ -1,16 +1,16 @@
-import * as browserslist from 'browserslist';
+import browserslist from 'browserslist';
 import {Capability, Options} from './types';
 import { getAllCapabilities, filterCapabilities } from './capabilities';
-
-
 
 export default async function getCapabilities(options?: Options): Promise<Capability[]> {
     const allCapabilities = await getAllCapabilities(options.username, options.accessKey);
     const allSupportedBrowsers = browserslist(options.browserslist.queries, options.browserslist.opts)
-        .map((browserString) => {
-            const [browser, browser_version] = browserString.split(' ');
-            return {browser, browser_version};
-        });
-
+    .map((browserString: string) => {
+        const [browser, browser_version] = browserString.split(' ');
+        return {browser, browser_version: (!browser_version.includes('.') && !isNaN(parseFloat(browser_version))) ? `${browser_version}.0` : browser_version};
+    });
+    
     return filterCapabilities(allCapabilities, allSupportedBrowsers, options);
 }
+
+export { BrowserFilter, OperatingSystemFilter, Options } from './types';
