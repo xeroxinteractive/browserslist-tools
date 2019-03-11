89831b6201a20711a59c2fc084b3f97e86a1809f
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-03-11 16:54:39 +0000

    test(index): overhaul tests

--- source/__tests__/index.test.ts
+++ source/__tests__/index.test.ts
@@ -1,8 +1,9 @@
 import * as moduleUnderTest from '../index';
+import BrowserslistError from 'browserslist/error';
 
 describe('getCapabilities', () => {
-  describe('options', () => {
-    test('IE 10', async () => {
+  describe('browserslist options', () => {
+    test('single browser query', async () => {
       await expect(
         moduleUnderTest.default({
           browserslist: {
@@ -11,7 +12,43 @@ describe('getCapabilities', () => {
         })
       ).resolves.toMatchSnapshot();
     });
-    test('IE 10 + Windows 7 OS filter', async () => {
+
+    test('multiple browser query', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: ['IE 10', 'Edge 18']
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+
+    test('last 2 versions', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: 'last 2 versions'
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+
+    test('invalid query', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: 'invalid'
+          }
+        })
+      ).rejects.toEqual(
+        new BrowserslistError(
+          'Unknown browser query `invalid`. Maybe you are using old Browserslist or made typo in query.'
+        )
+      );
+    });
+  });
+  describe('OS Filtering', () => {
+    test('os + os version include filters', async () => {
       await expect(
         moduleUnderTest.default({
           browserslist: {
@@ -21,7 +58,100 @@ describe('getCapabilities', () => {
             include: [moduleUnderTest.OperatingSystemFilter.WINDOWS]
           },
           operatingSystemVersion: {
-            include: [moduleUnderTest.WindowsOperatingSystemVersionFilter.SEVEN]
+            include: [
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.SEVEN,
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.TEN,
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.XP
+            ]
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+
+    test('os + os version exclude filters', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: 'Firefox 42'
+          },
+          operatingSystems: {
+            exclude: [moduleUnderTest.OperatingSystemFilter.OSX]
+          },
+          operatingSystemVersion: {
+            exclude: [
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.EIGHT_ONE,
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.EIGHT
+            ]
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+
+    test('os + os version include + exclude filters', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: 'Firefox 42'
+          },
+          operatingSystems: {
+            include: [moduleUnderTest.OperatingSystemFilter.OSX],
+            exclude: [moduleUnderTest.OperatingSystemFilter.WINDOWS]
+          },
+          operatingSystemVersion: {
+            exclude: [
+              moduleUnderTest.OSXOperatingSystemVersionFilter.LION,
+              moduleUnderTest.OSXOperatingSystemVersionFilter.HIGH_SIERRA,
+              moduleUnderTest.WindowsOperatingSystemVersionFilter.EIGHT
+            ]
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+  });
+
+  describe('browser filtering', () => {
+    test('browser include filter', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: ['Chrome 72', 'Opera 12', 'Edge 18', 'IE 10']
+          },
+          browsers: {
+            include: [
+              moduleUnderTest.BrowserFilter.EDGE,
+              moduleUnderTest.BrowserFilter.IE
+            ]
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+    test('browser exclude filter', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: ['Chrome 72', 'Opera 12', 'Edge 18', 'IE 10']
+          },
+          browsers: {
+            exclude: [
+              moduleUnderTest.BrowserFilter.OPERA,
+              moduleUnderTest.BrowserFilter.CHROME
+            ]
+          }
+        })
+      ).resolves.toMatchSnapshot();
+    });
+    test('browser include + exclude filter', async () => {
+      await expect(
+        moduleUnderTest.default({
+          browserslist: {
+            queries: ['Chrome 72', 'Opera 12', 'Edge 18', 'IE 10']
+          },
+          browsers: {
+            include: [moduleUnderTest.BrowserFilter.EDGE],
+            exclude: [
+              moduleUnderTest.BrowserFilter.OPERA,
+              moduleUnderTest.BrowserFilter.CHROME
+            ]
           }
         })
       ).resolves.toMatchSnapshot();
